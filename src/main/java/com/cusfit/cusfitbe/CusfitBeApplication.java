f3baa0a713dc7e285a04b1b6a4df47c96c1524e7
leesuyong849 <toosee1005@cau.ac.kr>
2026-09-03 00:34:34 +0900

    Spring Boot 프로젝트 스켈레톤 초기화
    이전 프로젝트(deoham) build.gradle 구성을 참고하여 group만 com.cusfit으로
    바꾸고 동일한 의존성/버전으로 세팅. 로컬 개발용 docker-compose(Postgres+Redis)와
    .env.example 추가, .gitignore를 Java/Gradle 기준으로 교체.

---
+++ src/main/java/com/cusfit/cusfitbe/CusfitBeApplication.java
@@ -0,0 +1,13 @@
+package com.cusfit.cusfitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusfitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CusfitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cusfit/cusfitbe/CusfitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cusfit.cusfitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CusfitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
